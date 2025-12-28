067c724d2c2e29ffd12732e1532204080be578b1
Tan <trantrongtan096@gmail.com>
2025-12-28 09:57:32 +0700

    Update: Danh sách ngân hàng

---
+++ src/main/java/vn/hoadon/dto/bank/BankFilterDTO.java
@@ -0,0 +1,22 @@
+package vn.hoadon.dto.bank;
+
+public class BankFilterDTO {
+	private String keyword;
+	private Integer status;
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+}
--- src/main/java/vn/hoadon/repositories/BankRepository.java
+++ src/main/java/vn/hoadon/repositories/BankRepository.java
@@ -1,12 +1,14 @@
 package vn.hoadon.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
 import vn.hoadon.entity.BankEntity;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface BankRepository extends JpaRepository<BankEntity, Long> {
+public interface BankRepository extends JpaRepository<BankEntity, Long>, JpaSpecificationExecutor<BankEntity>  {
     List<BankEntity> findByStatus(Integer status);
     List<BankEntity> findByAbbreviation(String abbreviation);
     List<BankEntity> findByName(String name);
